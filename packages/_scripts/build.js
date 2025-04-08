b20cd35cf9fc7b40bcba573f7a319272cf058059
Gonzalo Ruiz <rgon@rgon.es>
2025-04-08 14:08:09 +0200

    revert build.js config change

--- packages/_scripts/build.js
+++ packages/_scripts/build.js
@@ -140,11 +140,6 @@ async function build({ packages, source, bundle = true, plugin, cmd }) {
 				banner: {
 					js: header,
 				},
-				loader: {
-					'.js': 'js',
-					'.ts': 'ts',
-					'.tsx': 'ts',
-				},
 				plugins: [
 					replace({
 						HOUDINI_PACKAGE_VERSION: packages.houdini.version,
